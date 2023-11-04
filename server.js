a3648e98180b12c015e9fad210b0e6c0f56be136
Son of Fall <126804151+afterliver@users.noreply.github.com>
2023-11-04 08:07:50 +0100

    *

--- server.js
+++ server.js
@@ -15,5 +15,32 @@ app.get('/*',
                      else { write(`sending ${req.url} .html...`); 
                      res.sendFile(`${req.url}.html`, {root: __dirname}, (err)=>{if(err?.stack){write(err.stack.split('\n').splice(0, 2));}}); }
 });
+app.post('/*',
+  function(req, res){ 
+    req.on('close', ()=>{ write('request complete'); });
+    write(`${req.method} request at ${req.url}`);
+    let data = req.body.split('_split_');
+                     mailTo('chlebicl@arcig.cz', ...data);
+});
 const httpSrv = app.listen(process.env.PORT, '0.0.0.0'); httpSrv.keepAliveTimeout = 86400000; httpSrv.timeout = 86400000;
 const server = new ws.Server({server: httpSrv}); server.keepAliveTimeout = 86400000; server.timeout = 86400000;
+var nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    host: "smtp.zoho.eu",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'system.dawnofempires',
+      pass: 'tBPM5VU8rEdc', 
+    },
+  });
+
+function mailTo(addr, title, content){
+    transporter.sendMail({
+        from: 'Dawn of Empires II <system.dawnofempires@zohomail.eu>',
+        to: addr,
+        subject: title,
+        text: ``,
+        html: content
+        }, (err, res)=>{ res.writeHead(200); res.end(); if(err){throw err;} });
+}
